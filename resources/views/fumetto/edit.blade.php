8a00d72c19b95ad79ed7b57a260606897c89d7ef
marco maione <maionemarco14@gmail.com>
2022-04-04 19:13:50 +0200

    aggiornamento

--- resources/views/fumetto/edit.blade.php
+++ resources/views/fumetto/edit.blade.php
@@ -1,25 +1,30 @@
-@extends('layouts.base')
+ends('layouts.base')
 
-@section('pageTitle', 'crea fumetto')
+@section('pageTitle')
+
+    modifica:{{$fumetto->title}}
+
+@endsection
 
 @section('content')
 
     <div class="container">
-        <form method="POST" action="{{route('fumetto.store')}}">
+        <form method="POST" action="{{route('fumetto.update', ['fumetto' => $fumetto->id])}}">
 
             @csrf
+            @method('PUT')
 
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">immagine fumetto</label>
-                <input type="text" class="form-control" id="thumb" name="thumb">
+                <input type="text" class="form-control" id="thumb" name="thumb" value="{{$fumetto->thumb}}">
             </div>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">nome fumetto</label>
-                <input type="text" class="form-control" id="title" name="title">
+                <input type="text" class="form-control" id="title" name="title" value="{{$fumetto->title}}">
             </div>
             <div class="mb-3">
                 <div class="form-floating">
-                    <select class="form-select" id="type" name="type">
+                    <select class="form-select" id="type" value="{{$fumetto->type}}">
                         <option value="Comic book">Comic book</option>
                         <option value="Graphic novel">Graphic novel</option>
                     </select>
@@ -28,23 +33,23 @@
             </div>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">serie</label>
-                <input type="text" class="form-control" id="series" name="series">
+                <input type="text" class="form-control" id="series" name="series" value="{{$fumetto->series}}">
             </div>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">data di vendita</label>
-                <input type="text" class="form-control" id="sale_date" name="sale_date">
+                <input type="text" class="form-control" id="sale_date" name="sale_date" value="{{$fumetto->sale_date}}">
             </div>
             <div class="mb-3">
                 <div class="form-floating">
-                    <textarea class="form-control" placeholder="descrizione" id="description" name="description"></textarea>
+                    <textarea class="form-control" placeholder="descrizione" id="description" name="description">{{$fumetto->description}}</textarea>
                 </div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">prezzo</label>
-                <input type="text" class="form-control" id="price" name="price">
+                <input type="text" class="form-control" id="price" name="price" value="{{$fumetto->price}}">
             </div>
 
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Modifica</button>
         </form>
     </div>
     
