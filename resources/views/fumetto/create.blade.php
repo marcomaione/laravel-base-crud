8859b6846944e253202fc4517d308d70b9a0bce4
marco maione <maionemarco14@gmail.com>
2022-04-04 17:27:59 +0200

    aggiornamento

--- resources/views/fumetto/create.blade.php
+++ resources/views/fumetto/create.blade.php
@@ -4,38 +4,44 @@
 
 @section('content')
 
-    <form method="POST" action="{{route('fumetto.store')}}">
+    <div class="container">
+        <form method="POST" action="{{route('fumetto.store')}}">
 
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">nome fumetto</label>
-            <input type="text" class="form-control" id="title" name="title">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">tipo fumetto</label>
-            <input type="text" class="form-control" id="type" name="type">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">serie</label>
-            <input type="text" class="form-control" id="series" name="series">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">data di vendita</label>
-            <input type="text" class="form-control" id=" sale_date" name="sale_date">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">descrizione</label>
-            <input type="text" class="form-control" id="description" name="description">
-        </div>
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">prezzo</label>
-            <input type="text" class="form-control" id="price" name="price">
-        </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">nome fumetto</label>
+                <input type="text" class="form-control" id="title" name="title">
+            </div>
+            <div class="mb-3">
+                <div class="form-floating">
+                    <select class="form-select" id="type">
+                        <option value="Comic book">Comic book</option>
+                        <option value="Graphic novel">Graphic novel</option>
+                    </select>
+                    <label for="floatingSelect">tipo</label>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">serie</label>
+                <input type="text" class="form-control" id="series" name="series">
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">data di vendita</label>
+                <input type="text" class="form-control" id=" sale_date" name="sale_date">
+            </div>
+            <div class="mb-3">
+                <div class="form-floating">
+                    <textarea class="form-control" placeholder="descrizione" id="description" name="description"></textarea>
+                </div>
+            </div>
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">prezzo</label>
+                <input type="text" class="form-control" id="price" name="price">
+            </div>
 
-        <button type="submit" class="btn btn-primary">Submit</button>
-
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
     
-
-    </form>
 
 
         
